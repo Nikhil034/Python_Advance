303c6942579df9dbe79e2ce4e3ac1d184486ee18
Nikhil034 <nikhilsondarava342@gmail.com>
2021-12-23 19:29:28 +0530

    Attribute of numpy

---
+++ numempty.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+'''arr=np.array([(10,20,30),(10,20,30)])
+arr.shape(2,3)
+print(arr)'''
+
+'''arr=np.array([10.0,20.0,30.0],dtype=np.float64)
+print(arr.itemsize)'''
+
+'''arr=np.empty((3,3),int)
+print(arr)'''
+
+arr=np.zeros((3,3),int)
+print(arr)
+
+
