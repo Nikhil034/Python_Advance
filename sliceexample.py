e9b99fa6102919bbf3537d034b733df0a9cdf60d
Nikhil034 <nikhilsondarava342@gmail.com>
2021-12-29 21:09:52 +0530

    Slicing example

---
+++ sliceexample.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+'''a=np.arange(20)
+print(a[2:20:3])
+print(a[4])
+print(a[2:5])
+print(a[-1])
+print(a[-4:-1])
+print(a[4:-2])'''
+
+'''arr=np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(arr)
+print(arr[2,0])
+print(arr[1,1])'''
+
+arr=np.array([[1,2,3],[3,4,5],[4,5,6]])
+print(arr)
+
+print(arr[...,2])
+print(arr[1,...])
+
